d6c02392bb7046c954b255690a10c7b9f9b20976
rafaelindraid <iendhra@gmail.com>
2023-10-02 17:31:25 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -9,12 +9,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tgl = $_POST["tgl"];
 
     // Memanggil stored procedure
-    $sql = "CALL InsertTransaksi(?, ?, ?)";
+    $sql = "CALL InsertTransaksi(?, ?, ?, @new_id)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("iis", $id_produk, $jumlah, $tgl);
 
     if ($stmt->execute()) {
-        echo "Data transaksi berhasil ditambahkan.";
+        // Mengambil hasil dari stored procedure (new_id)
+        $select_result = $conn->query("SELECT @new_id AS new_id");
+        $result_row = $select_result->fetch_assoc();
+        $new_transaction_id = $result_row["new_id"];
+
+        // Menampilkan data yang baru saja diinputkan
+        if ($new_transaction_id != -1) {
+            $new_transaction_sql = "SELECT * FROM transaksi WHERE id_transaksi = $new_transaction_id";
+            $result = $conn->query($new_transaction_sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "Data transaksi berhasil ditambahkan:<br>";
+                    echo "ID Transaksi: " . $row["id_transaksi"] . "<br>";
+                    echo "ID Produk: " . $row["id_produk"] . "<br>";
+                    echo "Jumlah: " . $row["jumlah"] . "<br>";
+                    echo "Total: " . $row["total"] . "<br>";
+                    echo "Tanggal: " . $row["tgl"] . "<br>";
+                }
+            } else {
+                echo "Data transaksi tidak ditemukan.";
+            }
+        } else {
+            echo "Produk tidak ditemukan.";
+        }
     } else {
         echo "Error: " . $stmt->error;
     }
